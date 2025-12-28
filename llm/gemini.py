9cd86a02a6aad3263070241dc40a79d75bb88409
Yaroslav Likhachev <yaroslav_li07@mail.ru>
2025-12-29 02:28:59 +0300

    MVP realised

--- llm/gemini.py
+++ llm/gemini.py
@@ -1,10 +1,11 @@
 from google import genai
-from ..config import get_token_gemini
+from config import get_token_gemini
 
 
 PROMTE = """Проанализируй следующий текст и составь краткое содержание в формате списка с вложенными пунктами. Используй заголовок '#summary', затем перечисли ключевые темы обсуждения, каждую с новой строки, начиная с дефиса. Если в теме есть подпункты — оформи их с отступом в 2 пробела и также начни с дефиса. Сохраняй нейтральный и лаконичный тон, фокусируйся на сути обсуждений и действиях участников.
 Текст для анализа:"""
 API_key = get_token_gemini()
+print(API_key)
 client = genai.Client(api_key=API_key)
 
 
@@ -13,7 +14,8 @@ def send_promte(promte):
     response = client.models.generate_content(
         model="gemini-3-flash-preview", contents=PROMTE+promte
     )
-    return response
+    print(response.text)
+    return response.text
 
 
 
