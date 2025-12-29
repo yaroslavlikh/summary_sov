3a2dd852ba8398e7e86355537685d8975ce12ec6
Yaroslav Likhachev <yaroslav_li07@mail.ru>
2025-12-29 03:39:53 +0300

    final

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -13,18 +13,27 @@ def load_handlers(bot):
         cursor.execute("INSERT INTO messages (user_id, user_name, message) VALUES (?, ?, ?)",
                        (message.from_user.id, user_name, message.text))
         conn.commit()
+
         conn.close()
 
 
     @bot.message_handler(commands=['summary'])
-    def summary(message):
-        print("Создание краткого содержания")
+    def summary(message, limit: str = 150):
         conn = sqlite3.connect('database/messages.sql')
         cursor = conn.cursor()
-        cursor.execute("SELECT user_name, message FROM messages WHERE user_id = ?",
-                       (message.from_user.id,))
+        _ = message.text.split()
+        if len(_) > 1:
+            if _[1].isdigit():
+                limit = min(int(_[1]), int(cursor.execute("SELECT MAX(id) FROM messages").fetchone()[0]))
+
+        print("Создание краткого содержания")
+
+        cursor.execute("""SELECT user_name, message FROM messages WHERE user_id = ?
+                          ORDER BY id DESC 
+                          LIMIT ?""",
+                       (message.from_user.id, limit))
         messages = cursor.fetchall()
         conn.close()
         prompt = ". ".join([f"{msg[0]}: {msg[1]}" for msg in messages])
-        res = f"{send_promte(prompt)}\n Ну и Гордей хуесос"
+        res = f"{send_promte(prompt)}\n - Ну и Гордей хуесос"
         bot.send_message(message.chat.id, res)
