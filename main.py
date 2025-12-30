85783119cd46c2a6008670ab1ecd22022cf82cbb
Yaroslav Likhachev <yaroslavlikh@Yaroslavs-MacBook-Air.local>
2025-12-30 22:15:51 +0300

    add soket

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ import config
 from handlers.handlers import load_handlers
 from database.init_db import init_db
 
+import requests
+
+import socket
+socket.setdefaulttimeout(30)
+socket.has_ipv6 = False
+
+print("TEST TELEGRAM CONNECT")
+print(requests.get("https://api.telegram.org").status_code)
+
+
 def start_up():
     try:
         init_db()
 
+        telebot.apihelper.SESSION = None
+        telebot.apihelper.API_URL = "https://api.telegram.org/bot{0}/{1}"
         TELEGRAM_TOKEN = config.get_key_bot()
         bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
