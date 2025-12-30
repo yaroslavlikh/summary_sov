f7d56444badf40158614272bb9f7901ef51ad330
Yaroslav Likhachev <yaroslav_li07@mail.ru>
2025-12-30 23:26:48 +0300

    switch to webhook

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import telebot
+from flask import Flask, request
 import config
 from handlers.handlers import load_handlers
-from database.init_db import init_db
 
-def start_up():
-    try:
-        init_db()
+TOKEN = config.get_key_bot()
+WEBHOOK_URL = config.get_webhook_url()
 
-        TELEGRAM_TOKEN = config.get_key_bot()
-        bot = telebot.TeleBot(TELEGRAM_TOKEN)
+bot = telebot.TeleBot(TOKEN)
+load_handlers(bot)
 
-        load_handlers(bot)
+app = Flask(__name__)
 
-        print("Бот запущен")
-        bot.remove_webhook()
-        bot.infinity_polling(
-            timeout=60,
-            long_polling_timeout=60,
-            skip_pending=True
-        )
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    json_str = request.get_data().decode("UTF-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "OK", 200
 
 
-    except Exception as e:
-        print(f"Ошибка запуска: {e}")
+@app.route("/")
+def index():
+    return "Bot is alive", 200
 
-if __name__ == '__main__':
-    start_up()
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL)
+
+    print("Webhook установлен:", WEBHOOK_URL)
+
+    app.run(host="0.0.0.0", port=8080)
