77dfba735be3424b176e587206bd4f769cfa0e6b
Yaroslav Likhachev <yaroslavlikh@Yaroslavs-MacBook-Air.local>
2025-12-30 13:56:35 +0300

    safety get tokens

--- config.py
+++ config.py
@@ -6,10 +6,16 @@ load_dotenv()
 
 
 def get_key_bot():
-    token = os.getenv('BOT_TOKEN')
+    try:
+        token = os.getenv('BOT_TOKEN')
+    except Exception as e:
+        print(f"Ошибка при получении токена бота: {e}\nУбедитесь, что вы запускаете проект из корневой папки и в .env есть токен")
     return token
 
 
 def get_token_gemini():
-    token = os.getenv('GEMINI_API_KEY')
+    try:
+        token = os.getenv('GEMINI_API_KEY')
+    except Exception as e:
+        print(f"Ошибка при получении токена gemini: {e}\nУбедитесь, что вы запускаете проект из корневой папки и в .env есть токен")
     return token
