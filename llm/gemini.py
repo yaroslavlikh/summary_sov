acbfdaa4879f06097418ac4e8db607352cd97ff0
Yaroslav Likhachev <yaroslav_li07@mail.ru>
2025-12-30 13:45:24 +0300

    add gemini 1.5 when 3 version doesn't work

--- llm/gemini.py
+++ llm/gemini.py
@@ -7,11 +7,21 @@ API_key = get_token_gemini()
 
 def send_promte(promte):
     client = genai.Client(api_key=API_key)
-    response = client.models.generate_content(
+    new = ""
+    try:
+        flag = False
+        response = client.models.generate_content(
         model="gemini-3-flash-preview", contents=prompt_for_llm + promte
-    )
+        )
+    except Exception as e:
+        if not flag:
+            flag = True
+            new = "Новая версия модели недоступна, перешел на старую"
+        response = client.models.generate_content(
+        model="gemini-1.5-flash", contents=prompt_for_llm + promte
+        )
     print(response.text)
-    return response.text
+    return new + response.text
 
 
 
