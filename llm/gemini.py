19c511ff46d5a570883658240e617f631f17bfcf
Yaroslav Likhachev <yaroslav_li07@mail.ru>
2025-12-30 13:48:16 +0300

    add commenting when llm change into 1.5 version

--- llm/gemini.py
+++ llm/gemini.py
@@ -5,23 +5,24 @@ from llm.prompt import prompt_for_llm
 API_key = get_token_gemini()
 
 
-def send_promte(promte):
+def send_prompt(prompt):
     client = genai.Client(api_key=API_key)
     new = ""
     try:
         flag = False
         response = client.models.generate_content(
-        model="gemini-3-flash-preview", contents=prompt_for_llm + promte
+        model="gemini-3-flash-preview", contents=prompt_for_llm + prompt
         )
     except Exception as e:
         if not flag:
             flag = True
             new = "Новая версия модели недоступна, перешел на старую"
         response = client.models.generate_content(
-        model="gemini-1.5-flash", contents=prompt_for_llm + promte
+        model="gemini-1.5-flash", contents=prompt_for_llm + prompt
         )
     print(response.text)
-    return new + response.text
+    if new: return f'{new}\n{response.text}'
+    else: return response.text
 
 
 
