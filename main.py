3cdf36f1f119630ae7efe390fbb3cafcc9d01e58
Yaroslav Likhachev <yaroslavlikh@Yaroslavs-MacBook-Air.local>
2025-12-30 15:57:09 +0300

    ready for deploying in USA VPS

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-import os
-import config
 import telebot
+import config
 from handlers.handlers import load_handlers
 from database.init_db import init_db
-
 
 def start_up():
     try:
         init_db()
+
         TELEGRAM_TOKEN = config.get_key_bot()
         bot = telebot.TeleBot(TELEGRAM_TOKEN)
-        load_handlers(bot)
-        print("Бот запущен")
-        bot.polling()
-    except Exception as e:
-        print(f"Произошла ошибка при запуске бота: {e}")
 
+        load_handlers(bot)
+
+        print("Бот запущен")
+        bot.infinity_polling()
+
+    except Exception as e:
+        print(f"Ошибка запуска: {e}")
 
 if __name__ == '__main__':
     start_up()
