92200f4e51d6ce2c5842f0b7b947a12798fb4b65
Yaroslav Likhachev <yaroslavlikh@Yaroslavs-MacBook-Air.local>
2026-01-23 23:38:26 +0300

    realized logger error when key not in env

--- config.py
+++ config.py
@@ -8,14 +8,20 @@ load_dotenv()
 def get_key_bot():
     try:
         token = os.getenv('BOT_TOKEN')
+        if not token:
+            print("Ошибка: BOT_TOKEN не найден в .env файле")
+        return token
     except Exception as e:
         print(f"Ошибка при получении токена бота: {e}\nУбедитесь, что вы запускаете проект из корневой папки и в .env есть токен")
-    return token
+        return None
 
 
 def get_token_gemini():
     try:
         token = os.getenv('GEMINI_API_KEY')
+        if not token:
+            print("Ошибка: GEMINI_API_KEY не найден в .env файле")
+        return token
     except Exception as e:
         print(f"Ошибка при получении токена gemini: {e}\nУбедитесь, что вы запускаете проект из корневой папки и в .env есть токен")
-    return token
+        return None
